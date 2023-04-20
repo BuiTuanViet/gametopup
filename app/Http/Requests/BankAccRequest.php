adf24182eead481fbe5da6bc6503dff8faf358b9
vietbt <vietbt@vatgia.com>
2023-04-20 22:43:03 +0700

    fox

--- app/Http/Requests/BankAccRequest.php
+++ app/Http/Requests/BankAccRequest.php
@@ -26,7 +26,7 @@ class BankAccRequest extends FormRequest
         return [
             'acc_name' => [
                 'required',
-                'regex:/^[A-Z ]+$/'
+                'regex:/^[A-Za-z ]+$/'
             ],
             'acc_no' => 'required|numeric',
             'bank_no' => 'required',
